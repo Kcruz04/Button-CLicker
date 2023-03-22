78fdfd922fa0d202601ddeaf6d19bd6dbe2d93f4
Kcruz04 <kencruz515@gmail.com>
2023-03-21 18:10:10 -1000

    Button Assignment

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function remove(element){
+    element.remove()
+    console.log('remove')
+}
+
+function logoff(element){
+    element.innerText = "Lof Out";
+}
+function liked(element){
+    alert("Ninja was liked")
+    console.log("liked")
+}
